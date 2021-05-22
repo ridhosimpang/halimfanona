6c9b3e0f156d3509cab67da6281592beb70e5acd
ridhosimpang <ridohermawansyah13@gmail.com>
2021-05-22 22:37:41 +0700

    ganti text

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,7 +7,7 @@
 
 <section id="hero" class="d-flex align-items-center">
     <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
-      <h1>SELAMAT DATANG {{auth()->user()->name}}</h1>
+      <h1>Selamat Datang {{auth()->user()->name}}</h1>
     </div>
   </section>
 @endsection
--- resources/views/detailkonsumen.blade.php
+++ resources/views/detailkonsumen.blade.php
@@ -57,7 +57,15 @@
                         </div>
                       </div>
                     </div>
-                    
+                    <div class="col">
+                      <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                      </div>
+                    </div>
             
                     </div>
                     <a href="/ubahkonsumen/{{$konsumen->id}}/edit" class="btn btn-info">Edit</a>
