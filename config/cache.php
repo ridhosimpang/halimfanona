62cb0ab2ca57c0ef1ce0a4b7d34d91f1065911ac
nopiarahman <nopiarahman93@gmail.com>
2021-05-23 00:01:44 +0700

    benerin error

--- config/cache.php
+++ config/cache.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Str;
 return [
 
     /*
@@ -85,10 +85,6 @@ return [
     | value to get prefixed to all our keys so we can avoid collisions.
     |
     */
-
-    'prefix' => env(
-        'CACHE_PREFIX',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_cache'
-    ),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
 
 ];
